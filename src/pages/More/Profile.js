ea29aadfb24cade72da583673cf050eeb605f6fb
Buddhika Nipun <bnsbuddhika@gmail.com>
2023-08-27 23:17:56 +0530

    Did some profile changes

--- src/pages/More/Profile.js
+++ src/pages/More/Profile.js
@@ -8,46 +8,48 @@ function Profile() {
     <div className="Profile">
       <Navbar img={loginNav} />
 
-      <div className="container" style={{ padding: '6em', display: 'flex', justifyContent: 'center' }}>
-        <div className="row" style={{ maxWidth: '800px', margin: '0 auto' }}>
-          <div className="col-md-4 col-lg-3 text-center">
-            <div className="nav-buttons mb-4">
-              <Link to="/profile" className="nav-button-main">Profile</Link>
-            </div>
-          </div>
+      {/* <div className="text-center">
+        <div className="nav-buttons">
+          <Link to="/profile" className="nav-button-main">Profile</Link>
+        </div>
+      </div> */}
 
-          <div className="col-md-8 col-lg-9 sub-container" style={{ border: '1px solid black', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-            <div className="row" style={{ border: '1px solid red', width: '100%' }}>
-              <div className="col-md-6">
-                <img src={profile} className="profile-img img-fluid" alt="Profile" />
+      <div className="container mb-5 mt-5" style={{ padding: '4em 1em 4em 1em', display: 'flex', justifyContent: 'center'}}>
+        <div className="row d-flex justify-content-center" style={{ maxWidth: '800px', margin: '0 auto'}}>
+          {/* <div className="col" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            <div className="row"> */}
+              <div className="col-xl-6 d-flex justify-content-center">
+                <img src={profile} className="img-fluid" alt="Profile" />
               </div>
-              <div className="col-md-6 mt-4 mt-md-0">
-                <table className="table">
-                  <tbody>
-                    <tr>
-                      <td>Name</td>
-                      <td>Alex Thompson</td>
-                    </tr>
-                    <tr>
-                      <td>Age</td>
-                      <td>6</td>
-                    </tr>
-                    <tr>
-                      <td>Country</td>
-                      <td>Sri Lanka</td>
-                    </tr>
-                    <tr>
-                      <td>Level</td>
-                      <td>Level 2</td>
-                    </tr>
-                  </tbody>
-                </table>
+              <div className="col-xl-6 mt-5 d-flex justify-content-center">
+                <div className='table-responsive'>
+                  <table className="table">
+                    <tbody>
+                      <tr>
+                        <th>Name</th>
+                        <td>Nirwan Rajapaksha</td>
+                      </tr>
+                      <tr>
+                        <th>Age</th>
+                        <td>6</td>
+                      </tr>
+                      <tr>
+                        <th>Country</th>
+                        <td>Sri Lanka</td>
+                      </tr>
+                      <tr>
+                        <th>Level</th>
+                        <td>Level 2</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
+    //   </div>
+    // </div>
   );
 }
 
