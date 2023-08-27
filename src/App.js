2a2eab50bb5b6e55019ef29f32a29b83c596d6b4
DasunThathsara <dasun.thathsara.sri@gmail.com>
2023-08-27 13:42:55 +0530

    Add search bar

--- src/App.js
+++ src/App.js
@@ -9,9 +9,12 @@ function App() {
     <div className="App">
         <div className="m-">
           <nav className="navbar navbar-expand-md navbar-dark">
-            <div className="container-fluid d-flex justify-content-start align-items-center">
+            <div className="container-fluid d-flex justify-content-between align-items-center">
               <Link to="/login" className="navbar-brand" style={{ fontFamily: "'Comic Sans MS', sans-serif", fontSize: '3em'}}>KiddeEd</Link>
-              <img src={loginNav} alt="Login" className="img-fluid" />
+              <div className="search-bar">
+                <input type="text" className="form-control" placeholder="Search" />
+                <span className="search-icon"><i className="bi bi-search"></i></span>
+              </div>
             </div>
           </nav>
         </div>
