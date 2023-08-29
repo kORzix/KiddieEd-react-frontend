a2fb674e89fe9d8c47e53519401ead0ee4169d95
DasunThathsara <dasun.thathsara.sri@gmail.com>
2023-08-29 08:09:08 +0530

    Update topnavbar

--- src/components/topnavbar.js
+++ src/components/topnavbar.js
@@ -1,5 +1,11 @@
 import * as React from "react";
 import { Link } from "react-router-dom";
+import './topnavbar.css';
+import worksheet from "../images/worksheet.icon.png";
+import game from "../images/game.icon.png";
+import lesson from "../images/lessons.icon.png";
+import plan from "../images/plans.icon.png";
+import more from "../images/more.icon.png";
 
 const Navbar = () => {
     return (
@@ -16,11 +22,11 @@ const Navbar = () => {
 
             <div className="side-nav-container">
                 <div className="side-nav-buttons" style={{margin: '0 1.5em 0 1.5em'}}>
-                    <Link to="/worksheet" className="side-nav-button">Worksheets</Link>
-                    <Link to="/games" className="side-nav-button">Games</Link>
-                    <Link to="/guided-lessons" className="side-nav-button">Guided Lessons</Link>
-                    <Link to="/lessons-plans" className="side-nav-button">Lesson Plans</Link>
-                    <Link to="/more" className="side-nav-button">More</Link>
+                    <Link to="/worksheet" className="side-nav-button"><img className="icon" src={worksheet} /></Link>
+                    <Link to="/games" className="side-nav-button"><img className="icon" src={game} /></Link>
+                    <Link to="/guided-lessons" className="side-nav-button"><img className="icon" src={lesson} /></Link>
+                    <Link to="/lessons-plans" className="side-nav-button"><img className="icon" src={plan} /></Link>
+                    <Link to="/more" className="side-nav-button"><img className="icon" src={more} /></Link>
                 </div>
             </div>
         </div>
