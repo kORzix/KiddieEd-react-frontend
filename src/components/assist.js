1a8ac2ff2fa8ba57b86da794248fd505d9f22d29
DasunThathsara <dasun.thathsara.sri@gmail.com>
2023-09-07 19:23:57 +0530

    Update assistant

--- src/components/assist.js
+++ src/components/assist.js
@@ -2,6 +2,19 @@ import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognitio
 import useClipboard from "react-use-clipboard";
 import {useState} from "react";
 
+function buttonToggle() {
+    var y = document.getElementById("start-btn");
+    var x = document.getElementById("stop-btn");
+    if (y.style.display === "none") {
+        y.style.display = "grid";
+        x.style.display = "none";        
+        SpeechRecognition.stopListening();
+    } else {
+        y.style.display = "none";
+        x.style.display = "grid";
+        SpeechRecognition.startListening({ continuous: true, language: 'en-IN' });
+    }
+}
 
 const Assist = () => {
     const [textToCopy, setTextToCopy] = useState();
@@ -9,7 +22,7 @@ const Assist = () => {
         successDuration:1000
     });
 
-    const startListening = () => SpeechRecognition.startListening({ continuous: true, language: 'en-IN' });
+    // const startListening = () => ;
     const { transcript, browserSupportsSpeechRecognition } = useSpeechRecognition();
 
     if (!browserSupportsSpeechRecognition) {
@@ -17,26 +30,23 @@ const Assist = () => {
     }
 
     return (
-        <>
-            <div className="container">
+        <div className="container">
 
-                <div className="main-content" onClick={() =>  setTextToCopy(transcript)}>
-                    {transcript}
-                </div>
+            <div className="main-content" onClick={() =>  setTextToCopy(transcript)}>
+                {transcript}
+            </div>
 
-                <div className="btn-style">
+            <div className="btn-style">
 
-                    <button onClick={setCopied}>
-                        {isCopied ? 'Copied!' : 'Copy to clipboard'}
-                    </button>
-                    <button onClick={startListening}>Start Listening</button>
-                    <button onClick={SpeechRecognition.stopListening}>Stop Listening</button>
-
-                </div>
+                {/* <button onClick={setCopied}>
+                    {isCopied ? 'Copied!' : 'Copy to clipboard'}
+                </button> */}
+                <button className="assist-start-btn" onClick={buttonToggle} id="start-btn">Start Listening</button>
+                <button className="assist-stop-btn" onClick={buttonToggle} id="stop-btn" style={{display:'none'}}>Stop Listening</button>
 
             </div>
 
-        </>
+        </div>
     );
 };
 
