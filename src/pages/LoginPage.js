152223e81d6ce4b6bce3281929e333c202973d82
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2023-11-18 00:49:07 +0530

    Authentication Done in JWT. But still some error

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,53 +1,66 @@
 import React, { useState } from "react";
 import loginImage from '../images/login-b.png';
 import loginNav from '../images/login-nav-img.png';
-import { useNavigate, Link } from "react-router-dom";
-import { toast } from 'react-toastify'
-import axios from 'axios';
-import { PROXY } from "../configs";
+import { Link } from "react-router-dom";
+import { useLogin } from '../hooks/useLogin';
 
 function LoginPage() {
-    const [formData, setFormData] = useState({
-        userEmail: '',
-        userPassword: '',
-      })
+    // const [formData, setFormData] = useState({
+    //     userEmail: '',
+    //     userPassword: '',
+    //   })
 
-      const { userEmail, userPassword } = formData
-    
-      const navigate = useNavigate()
+    //   const { userEmail, userPassword } = formData
 
-      const onChange = (e) => {
-        setFormData((prevState) => ({
-          ...prevState,
-          [e.target.name]: e.target.value,
-        }))
-      }
+    //   const navigate = useNavigate()
 
-      const onSubmit = (e) => {
-        e.preventDefault()
-    
-        const userData = {
-          userEmail,
-          userPassword,
-        }
+    //   const onChange = (e) => {
+    //     setFormData((prevState) => ({
+    //       ...prevState,
+    //       [e.target.name]: e.target.value,
+    //     }))
+    //   }
 
-        axios.post(PROXY+"/user/login", userData)
-            .then((res) => {
-            console.log("Response from server:", res.data);
-            if (res.data) {
-                console.log("User id:", res.data._id);
-                console.log("Login successful!");
-                toast.success("Login successful!");
-                navigate("../");
-            } else {
-                toast.error(res.data.message); 
-            }
-            })
-            .catch((error) => {
-            console.error('Login error:', error);
-            toast.error('Login failed!');
-            });
-      }
+    //   const onSubmit = (e) => {
+    //     e.preventDefault()
+
+    //     const userData = {
+    //       userEmail,
+    //       userPassword,
+    //     }
+
+    //     axios.post(PROXY+"/user/login", userData)
+    //         .then((res) => {
+    //         console.log("Response from server:", res.data);
+    //         if (res.data) {
+    //             console.log("User id:", res.data._id);
+    //             console.log("Login successful!");
+    //             toast.success("Login successful!");
+    //             navigate("../");
+    //         } else {
+    //             toast.error(res.data.message); 
+    //         }
+    //         })
+    //         .catch((error) => {
+    //         console.error('Login error:', error);
+    //         toast.error('Login failed!');
+    //         });
+    //   }
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const { login, error, isLoading } = useLogin();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        await login({ email, password });
+        clearForm();
+    };
+
+    function clearForm() {
+        setEmail('');
+        setPassword('');
+    }
 
     return (
         <div>
@@ -55,8 +68,8 @@ function LoginPage() {
                 <nav className="navbar navbar-expand-md navbar-dark">
                     <div className="container-fluid d-flex justify-content-start align-items-center">
                         <Link to="/login" className="navbar-brand"
-                              style={{fontFamily: "'Comic Sans MS', sans-serif", fontSize: '3em'}}>KiddieEd</Link>
-                        <img src={loginNav} alt="Login" className="img-fluid"/>
+                            style={{ fontFamily: "'Comic Sans MS', sans-serif", fontSize: '3em' }}>KiddieEd</Link>
+                        <img src={loginNav} alt="Login" className="img-fluid" />
                     </div>
                 </nav>
             </div>
@@ -65,13 +78,13 @@ function LoginPage() {
                 <div className="row">
                     {/* Left Column for Image */}
                     <div className="col-md-6 d-flex justify-content-end">
-                        <img src={loginImage} alt="Login" className="img-fluid"/>
+                        <img src={loginImage} alt="Login" className="img-fluid" />
                     </div>
                     {/* Right Column for Login Form */}
                     <div className="col-md-6">
-                        <div className="p-4 bg-white form" style={{borderRadius: '10px'}}>
+                        <div className="p-4 bg-white form" style={{ borderRadius: '10px' }}>
                             <h2>Sign In to your Account</h2>
-                            <form style={{margin: '3em 2em 1em 2em'}} onSubmit={onSubmit}>
+                            <form style={{ margin: '3em 2em 1em 2em' }} onSubmit={handleSubmit}>
                                 <div className="mb-3">
                                     <label htmlFor="useremail" className="form-label">Email</label>
                                     <input
@@ -80,9 +93,9 @@ function LoginPage() {
                                         id="useremail"
                                         placeholder='Email'
                                         name="userEmail"
-                                        value={userEmail}
+                                        value={email}
                                         required
-                                        onChange={onChange}
+                                        onChange={(e) => setEmail(e.target.value)}
                                     />
                                 </div>
                                 <div className="mb-3">
@@ -93,17 +106,18 @@ function LoginPage() {
                                         id="password"
                                         placeholder='Password'
                                         name="userPassword"
-                                        value={userPassword}
+                                        value={password}
                                         required
-                                        onChange={onChange}
+                                        onChange={(e) => setPassword(e.target.value)}
                                     />
                                 </div>
                                 <div className="submit-btn">
-                                    <button type="submit" className="btn btn-primary"
-                                            style={{padding: '0.2em 4em 0.2em 4em'}}>Login
+                                    <button type="submit" className="btn btn-primary" disabled={isLoading}
+                                        style={{ padding: '0.2em 4em 0.2em 4em' }}>Login
                                     </button>
                                 </div>
-                                <p>Don't have an account? <Link to="/register" style={{color: '#00B4D8'}}>Register</Link></p>
+                                <p>Don't have an account? <Link to="/register" style={{ color: '#00B4D8' }}>Register</Link></p>
+                                {error && <div className='errorLogin'>{error}</div>}
                             </form>
                         </div>
                     </div>
